b872aa8c9c936362e2e96f65fb09cdb65f1740af
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-03-28 19:39:30 +0100

    Added RelativePanel Sample

---
+++ RelativePanel/RelativePanel/RelativePanel/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RelativePanel
+{
+	using Windows.UI.Xaml.Controls;
+
+	public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
