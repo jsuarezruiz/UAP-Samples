fe54f488ca748959f59d98c4c2faacc49f68543d
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-04-02 20:09:06 +0200

    Added DeviceFamily View Sample

---
+++ DeviceFamily View/DeviceFamily View/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DeviceFamily_View
+{
+	using System.Diagnostics;
+	using Windows.UI.Xaml.Controls;
+
+	public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+		private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			Debug.WriteLine("Lógica compartida!");
+		}
+	}
+}
