0750ea052424f82d594c11b73f58704613e8d35e
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-04-06 19:24:01 +0200

    Extensions SDK Sample

---
+++ Extensions SDK/ExtensionsSDK/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ExtensionsSDK
+{
+	public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
