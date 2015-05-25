b4bcf32d6399101be1fb73fe159507a987d47b80
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-05-25 08:59:52 +0200

    HTTP Live Streaming

---
+++ HlsPlayer/HlsPlayer/Behaviors/MediaStreamSourceBehavior.cs
@@ -0,0 +1,51 @@
+﻿namespace HlsPlayer.Behaviors
+{
+    using Microsoft.Xaml.Interactivity;
+    using System;
+    using Windows.Media.Streaming.Adaptive;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public class MediaStreamSourceBehavior : DependencyObject, IBehavior
+    {
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            var control = associatedObject as MediaElement;
+            if (control == null)
+                throw new ArgumentException(
+                    "MediaStreamSourceBehavior can be attached only to MediaElement.");
+
+            AssociatedObject = associatedObject;
+        }
+
+        public void Detach()
+        {
+            AssociatedObject = null;
+        }
+
+        public AdaptiveMediaSource Media
+        {
+            get { return (AdaptiveMediaSource)GetValue(MediaProperty); }
+            set { SetValue(MediaProperty, value); }
+        }
+
+        public static readonly DependencyProperty MediaProperty =
+            DependencyProperty.Register("Media",
+            typeof(AdaptiveMediaSource),
+            typeof(MediaStreamSourceBehavior),
+            new PropertyMetadata(null, OnMediaChanged));
+
+        private static void OnMediaChanged(object sender,
+        DependencyPropertyChangedEventArgs e)
+        {
+            var behavior = sender as MediaStreamSourceBehavior;
+            if (behavior.AssociatedObject == null || e.NewValue == null) return;
+
+            var mediaElement = behavior.AssociatedObject as MediaElement;
+            if (mediaElement != null)
+                mediaElement.SetMediaStreamSource((AdaptiveMediaSource)e.NewValue);
+        }
+    }
+}
---
+++ HlsPlayer/HlsPlayer/ViewModels/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿namespace HlsPlayer.ViewModels
+{
+    using System.Threading.Tasks;
+    using Base;
+    using Windows.UI.Xaml.Navigation;
+    using System.Windows.Input;
+    using Windows.Media.Streaming.Adaptive;
+    using System;
+
+    public class MainViewModel : ViewModelBase
+    {
+        // Consts
+        private const string HlsUrl = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8";
+
+        // Variables
+        private AdaptiveMediaSource _mediaSource;
+
+        // Commands
+        private ICommand _playCommand;
+
+        public override Task OnNavigatedFrom(NavigationEventArgs args)
+        {
+            return null;
+        }
+
+        public override Task OnNavigatedTo(NavigationEventArgs args)
+        {
+            return null;
+        }
+
+        public AdaptiveMediaSource MediaSource
+        {
+            get { return _mediaSource; }
+            set
+            {
+                _mediaSource = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand PlayCommand
+        {
+            get { return _playCommand = _playCommand ?? new DelegateCommandAsync(PlayCommandExecute); }
+        }
+
+        private async Task PlayCommandExecute()
+        {
+            var hlsUri = new Uri(HlsUrl);
+            var hlsSource = await AdaptiveMediaSource.CreateFromUriAsync(hlsUri);
+
+            if (hlsSource.Status == AdaptiveMediaSourceCreationStatus.Success)
+            {
+                MediaSource = hlsSource.MediaSource;
+            }
+        }
+    }
+}
