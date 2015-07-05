8829b046a52de6552d9c0181affa5f8b53921781
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-07-05 21:52:05 +0200

    Updated samples

---
+++ XBindPerf/xBindPerformance/Views/XBind.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace xBindPerformance.Views
+{
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media;
+
+    public sealed partial class XBind : Page
+    {
+        public XBind()
+        {
+            this.InitializeComponent();
+        }
+
+        public SolidColorBrush BackgroundA
+        {
+            get { return App.Current.Resources["BackgroundA"] as SolidColorBrush; }
+        }
+
+        public SolidColorBrush BackgroundB
+        {
+            get { return App.Current.Resources["BackgroundB"] as SolidColorBrush; }
+        }
+
+        public SolidColorBrush BackgroundC
+        {
+            get { return App.Current.Resources["BackgroundC"] as SolidColorBrush; }
+        }
+
+        public SolidColorBrush BackgroundD
+        {
+            get { return App.Current.Resources["BackgroundD"] as SolidColorBrush; }
+        }
+    }
+}
