77b6d6b72ec632875ef96188ca14303abff39206
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-07-06 19:33:25 +0200

    UserInteractionMode Sample

---
+++ InteractionMode/InteractionMode/ViewModels/Base/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿namespace InteractionMode.ViewModels.Base
+{
+    using Microsoft.Practices.Unity;
+    using ViewModels;
+
+    public class ViewModelLocator
+    {
+        readonly IUnityContainer _container;
+
+        public ViewModelLocator()
+        {
+            _container = new UnityContainer();
+
+			// ViewModels
+            _container.RegisterType<MainViewModel>();
+		}
+
+        public MainViewModel MainViewModel
+        {
+            get { return _container.Resolve<MainViewModel>(); }
+        }
+	}
+}
---
+++ InteractionMode/InteractionMode/ViewModels/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿namespace InteractionMode.ViewModels
+{
+    using System.Threading.Tasks;
+    using Base;
+    using Windows.UI.Xaml.Navigation;
+    using Windows.UI.ViewManagement;
+
+    public class MainViewModel : ViewModelBase
+    {
+        // Variables
+        private string _userInteractionMode;
+
+        public string InteractionMode
+        {
+            get { return _userInteractionMode; }
+            set
+            {
+                _userInteractionMode = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public override Task OnNavigatedFrom(NavigationEventArgs args)
+        {
+            return null;
+        }
+
+        public override Task OnNavigatedTo(NavigationEventArgs args)
+        {
+            GetUserInteractiveMode();
+
+            return null;
+        }
+
+        private void GetUserInteractiveMode()
+        {
+            var userInteractiveMode = UIViewSettings.GetForCurrentView().UserInteractionMode;
+
+            switch (userInteractiveMode)
+            {
+                case UserInteractionMode.Mouse:
+                    InteractionMode = "Ratón";
+                    break;
+                case UserInteractionMode.Touch:
+                    InteractionMode = "Táctil";
+                    break;
+            }
+        }
+    }
+}
