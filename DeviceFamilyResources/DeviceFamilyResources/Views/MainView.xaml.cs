3a7ab8d1b6c085b3e00a41d891d93808db6c8434
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2015-11-09 21:18:08 +0100

    DeviceFamily Resources Sample

---
+++ DeviceFamilyResources/DeviceFamilyResources/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DeviceFamilyResources.Views
+{
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
