87c98e34914bf83cd582233704fa0e82bf4c8f6d
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2016-01-18 21:16:01 +0100

    Assets DPI Scale Sample

---
+++ AssetsDpiScale/AssetsDpiScale/ViewModels/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+using AssetsDpiScale.ViewModels.Base;
+using Windows.UI.Xaml.Navigation;
+using Windows.Graphics.Display;
+
+namespace AssetsDpiScale.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private DisplayInformation _displayInformation;
+        private string _logicalDpi;
+        private string _scale;
+
+        public MainViewModel()
+        {
+            _displayInformation = DisplayInformation.GetForCurrentView();
+        }
+
+        public string LogicalDpi
+        {
+            get { return _logicalDpi; }
+            set
+            {
+                _logicalDpi = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Scale
+        {
+            get { return _scale; }
+            set
+            {
+                _scale = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public override Task OnNavigatedFrom(NavigationEventArgs args)
+        {
+            _displayInformation.DpiChanged -= _displayInformation_DpiChanged;
+
+            return null;
+        }
+
+        public override Task OnNavigatedTo(NavigationEventArgs args)
+        {
+            UpdateDpi(_displayInformation);
+            _displayInformation.DpiChanged += _displayInformation_DpiChanged;
+
+            return null;
+        }
+
+        private void _displayInformation_DpiChanged(DisplayInformation sender, object args)
+        {
+            DisplayInformation displayInformation = sender as DisplayInformation;
+
+            UpdateDpi(displayInformation);
+        }
+
+        private void UpdateDpi(DisplayInformation displayInformation)
+        {
+            if (displayInformation != null)
+            {
+                LogicalDpi = displayInformation.LogicalDpi.ToString();
+                Scale = (displayInformation.RawPixelsPerViewPixel * 100.0).ToString();
+            }
+        }
+    }
+}
---
+++ AssetsDpiScale/AssetsDpiScale/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using AssetsDpiScale.Views.Base;
+
+namespace AssetsDpiScale.Views
+{
+    public sealed partial class MainView : PageBase
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
