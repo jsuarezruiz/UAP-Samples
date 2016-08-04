c60e42ec9e76d4fb3177f339f63c53f49dbb2645
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2016-08-04 21:23:30 +0200

    InkToolbar Sample

---
+++ InkToolbar Sample/InkToolbar Sample/Views/MainView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace InkToolbar_Sample.Views
+{
+    public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+
+            InkCanvas.InkPresenter.InputDeviceTypes = 
+                CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Touch;
+        }
+
+        public void OnClear()
+        {
+            InkCanvas.InkPresenter.StrokeContainer.Clear();
+        }
+    }
+}
