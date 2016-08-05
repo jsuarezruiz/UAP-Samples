8c6d37de9b446936c9dd7cae03215ff6cbdce888
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2016-08-05 21:30:55 +0200

    xBind with functions Sample

---
+++ xBind Functions/xBind Functions/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace xBind_Functions.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+       
+    }
+}
---
+++ xBind Functions/xBind Functions/Views/MainView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using xBind_Functions.Models;
+
+namespace xBind_Functions.Views
+{
+    public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+
+        public string UserPassword { get; set; }
+
+        public SolidColorBrush PasswordStrengthBrush(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return new SolidColorBrush(Colors.Transparent);
+
+            int length = password.Length;
+
+            if (length >= 3 && length < 6)
+                return new SolidColorBrush(Colors.Orange);
+            if (length >= 6)
+                return new SolidColorBrush(Colors.Green);
+
+            return new SolidColorBrush(Colors.Red);
+        }
+
+        public double PasswordStrengthValue(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return 0;
+
+            int length = password.Length;
+
+            if (length >= 3 && length < 6)
+                return 66;
+            if (length >= 6)
+                return 100;
+
+            return 33;
+        }
+    }
+}
