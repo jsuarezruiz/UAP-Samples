8f39a6937e6d771f97b5de5e37d949522983cc85
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2016-08-17 18:46:47 +0200

    Updated xBind Sample

--- xBind Functions/xBind Functions/Views/MainView.xaml.cs
+++ xBind Functions/xBind Functions/Views/MainView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using xBind_Functions.Models;
@@ -7,12 +9,43 @@ namespace xBind_Functions.Views
 {
     public sealed partial class MainView : Page
     {
+        private ObservableCollection<House> _houses;
+
         public MainView()
         {
             this.InitializeComponent();
-        }
 
+            // Init
+            IsVisible = true;
+            IsNotVisible = false;
+
+            Dictionary = new Dictionary<string, string>()
+            {
+                { "Clave", "Valor" }
+            };
+
+            Element = new TextBlock
+            {
+                Text = "Cool"
+            };
+        }
+               
         public string UserPassword { get; set; }
+        public bool IsVisible { get; set; }
+        public bool IsNotVisible { get; set; }
+        public Dictionary<string, string> Dictionary { get; set; }
+        public object Element { get; set; }
+
+        public ObservableCollection<House> Houses
+        {
+            get
+            {
+                if (_houses == null)
+                    LoadHouses();
+
+                return _houses;
+            }
+        }
 
         public SolidColorBrush PasswordStrengthBrush(string password)
         {
@@ -42,6 +75,20 @@ namespace xBind_Functions.Views
                 return 100;
 
             return 33;
+        }
+
+        private void LoadHouses()
+        {
+            _houses = new ObservableCollection<House>();
+
+            _houses.Add(new House
+            {
+                Name = "Home 1"
+            });
+            _houses.Add(new House
+            {
+                Name = "Home 2"
+            });
         }
     }
 }
